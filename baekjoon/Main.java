a7f4549ec755686454713ae5a9d63360f5ed6274
daun <wjdekdns0218@gmail.com>
2026-01-09 14:08:31 +0900

    feat(boj): 10807 개수세기 (배열)

---
+++ baekjoon/Main.java
@@ -0,0 +1,30 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        String[] input = br.readLine().split(" ");
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(input[i]);
+        }
+
+        int v = Integer.parseInt(br.readLine());
+
+        int result = 0;
+
+        for (int j = 0; j < n; j++) {
+            if(arr[j] == v) {
+                result++;
+            }
+        }
+
+        System.out.println(result);
+    }
+}
