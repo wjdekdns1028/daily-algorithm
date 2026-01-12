d24819632511c96683c2ae6ba665cd3b3fe28a5c
daun <wjdekdns0218@gmail.com>
2026-01-12 15:06:03 +0900

    feat(boj): 10798 세로읽기 (2차원 배열)

---
+++ baekjoon/boj_10798/Main.java
@@ -0,0 +1,27 @@
+package baekjoon.boj_10798;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] words = new String[5];
+        for(int i = 0; i < 5; i++) {
+            words[i] = br.readLine();
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int col = 0; col < 15; col++) {
+            for(int row = 0; row < 5; row++) {
+                if(col < words[row].length()) {
+                    sb.append(words[row].charAt(col));
+                }
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
