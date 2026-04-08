9c6b1c8e935fe69e0b8c40b6bda8421e6ec5d0e4
daun <wjdekdns0218@gmail.com>
2026-04-08 16:07:48 +0900

    feat(cote): 프로그래머스 6603 로또

---
+++ cote/boj_6603.java
@@ -0,0 +1,44 @@
+package cote;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class boj_6603 {
+
+    static void DFS(int[] ar, int index, List<Integer> selected, int r){
+        if(index >= ar.length){
+            if(selected.size() == r){
+                System.out.println(selected);
+            }
+            return;
+        }
+
+        selected.add(ar[index]);
+        DFS(ar, index + 1, selected, r);
+
+        selected.remove(selected.size() - 1);
+        DFS(ar, index + 1, selected, r);
+
+    }
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true) {
+            String[] arr = br.readLine().split(" ");
+            int k = Integer.parseInt(arr[0]);
+
+            if(k == 0) break;
+
+            int[] ar = new int[k];
+            for (int i = 0; i < k; i++) {
+                ar[i] = Integer.parseInt(arr[i + 1]);
+            }
+
+            DFS(ar, 0, new ArrayList<>(), 6);
+            System.out.println();
+        }
+    }
+}
